ed6b9a7c29c20c780f3cb54693acba83a2ba8732
AjayNarera <nareraajay6@gmail.com>
2024-06-04 13:29:45 +0530

    project commit

---
+++ testimonial.js
@@ -0,0 +1,58 @@
+<section class="relative">
+<div class="w-11/12 max-w-[1300px] mx-auto relative py-20">
+  <img src="./Images/feature-section1-dottedrows.png" 
+  class="absolute w-[200px] top-[7.8rem] left-[1rem] -z-[10]">
+  <!-- top-section -->
+   <div class="font-mullish font-bold text-2xl text-center text-deepBlueHead">An Experience <br>
+    People Love to Talk About</div>
+   <div class="h-1 w-6 bg-greenLight mx-auto mt-4 "></div>
+   
+   <!-- left-button -->
+   <button class="w-[100px] h-[100px] bg-[#f4f8ff] rounded-full absolute left-0 top-1/2
+   flex justify-center items-center ">
+    <div class="w-[65%] h-[65%] bg-[#f4f8ff] rounded-full flex justify-center items-center
+    mix-blend-multiply">
+      <img src="./Images/1000.jpg" class="w-10 h-10 mix-blend-multiply rotate-180 ">
+    </div>
+  </button>
+
+
+    <!-- right-button -->
+    <button class="w-[100px] h-[100px] bg-[#f4f8ff] rounded-full absolute right-0 top-1/2
+    flex justify-center items-center ">
+     <div class="w-[65%] h-[65%] bg-[#f4f8ff] rounded-full flex justify-center items-center
+     mix-blend-multiply">
+       <img src="./Images/1000.jpg" class="w-10 h-10 mix-blend-multiply  ">
+     </div>
+   </button>
+
+   <!-- main-section -->
+   <div class="flex  flex-raw items-center max-w-[960px] mx-auto my-20 space-x-[10rem]">
+       <!-- left-image -->
+       <img src="./Images/testimonial.jpg" width="320px" height="320px"
+       class="rounded-xl">
+       <!-- right-part -->
+       <div class="max-w-[400px] space-y-2 ">
+        <img src="./Images/quotes.svg" width="35px"
+        height="40px"
+        class="-mb-2">
+        <p class="font-mullish font-extralight opacity-80 text-3xl">Finance Your Working Capital To Continue Growing</p>
+        <a href="#" class="text-greyText underline italic">Learn More</a>
+        <h2 class="font-extrabold font-mullish text-2xl">Subham Kumar</h2>
+        <p class="font-medium font-mullish">Product Manager</p>
+        <img src="./Images/fake-company-logo.png" width="80px"
+        height="40px">
+       </div>
+      
+   </div>
+
+   <!-- bottom-section -->
+   <div class="flex flex-raw justify-center space-x-2">
+        <div class="bg-gray-300 w-[10px] h-[10px] rounded-full"></div>
+        <div class="bg-lightBlue300 w-[10px] h-[10px] rounded-full"></div>
+        <div class="bg-gray-300 w-[10px] h-[10px] rounded-full"></div>                         
+        <div class="bg-gray-300 w-[10px] h-[10px] rounded-full"></div>
+        <div class="bg-gray-300 w-[10px] h-[10px] rounded-full"></div>
+        <div class="bg-gray-300 w-[10px] h-[10px] rounded-full"></div> 
+  </div>
+</section>
